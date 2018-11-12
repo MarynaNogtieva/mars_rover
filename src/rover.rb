25d100e918cc634bb44d897fe68a6e692b6a077f
Maryna Nogtieva <marynanogtieva@yahoo.ca>
2018-11-12 16:00:29 -0500

    make method a bit more readable

--- src/rover.rb
+++ src/rover.rb
@@ -26,7 +26,10 @@ class Rover
 
   def make_a_move
     new_coordinate_values = Command.get_new_coordinate_values(current_direction)
-    potential_new_coordinate = current_coordinate.add_values(new_x: new_coordinate_values[:x], new_y: new_coordinate_values[:y])
+    potential_new_coordinate = current_coordinate.add_values(
+                                new_x: new_coordinate_values[:x],
+                                new_y: new_coordinate_values[:y]
+                              )
     if can_move?(plateau, potential_new_coordinate)
       self.current_coordinate = current_coordinate.change(x: new_coordinate_values[:x], y: new_coordinate_values[:y])
     end
